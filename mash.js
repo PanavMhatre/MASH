92a8d9a4a5368451a423a0949f45539ca7f6a404
PanavMhatre <mhatrepanav@gmail.com>
2024-08-01 15:35:28 -0500

    working function to get a random home in mash

--- mash.js
+++ mash.js
@@ -1,3 +1,25 @@
 function randNumGenerator(num){
     return Math.floor(Math.random() * (num));
 }
+function getHome(){
+    let home = ["Mansion", "Castle", "Shack", "House"];
+    let input = process.argv[2];
+    let split = randNumGenerator(101);
+    if(input === undefined){
+        let randNum = randNumGenerator(4);
+        if(randNum === 4){
+            console.log("You got really unlucky :(");
+        }
+        return home[randNum];
+    }else{
+        if(split >= 50){
+            return input;
+        }else{
+            let randNum = randNumGenerator(4);
+            if(randNum === 4){
+                console.log("You got really unlucky :(");
+            }
+            return home[randNum];
+        }
+    }
+}
